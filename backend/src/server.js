39d7c835af440b1746cb6e50fd45f5d962cc9c71
Daljit singh <diljit2164@gmail.com>
2025-10-19 19:26:29 +0530

    Enhance CORS configuration in server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,7 +12,8 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: true, credentials: true }));
+
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
